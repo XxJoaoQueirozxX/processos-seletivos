d2fce1a78901bca87c67c4918f8168cf41d1d63e
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-02-20 11:32:22 -0300

    adicionado comando para rodar os testes unitarios

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 from app import create_app, db
-
+import click
 
 load_dotenv()
 app = create_app("dev")
@@ -10,17 +10,31 @@ migrate = Migrate(app, db)
 
 @app.before_first_request
 def init_db():
+    """init the models on database"""
     db.create_all()
 
 
 @app.shell_context_processor
 def shell_context():
+    """Make the shell context for flask shell"""
     return dict(db=db)
 
 
 @app.route("/")
 def index():
     return f"<h1>Home</h1>"
+
+
+@app.cli.command()
+@click.argument('test_names', nargs=-1)
+def test(test_names):
+    """Run the unit tests."""
+    import unittest
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 if __name__ == '__main__':
